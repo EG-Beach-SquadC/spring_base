4f4f43b85d4b1c279afbf031ab1158fd53df6bb9
Weixiao Chen <weixiao.chen.98@foxmail.com>
2023-08-08 16:05:16 +0800

    [TL-2][Weixiao & Linhao] add flyway script to insert product exmaple record

---
+++ src/main/resources/db/migration/V202308081526__insert_product_example.sql
@@ -0,0 +1,6 @@
+insert into product(`id`, `name`, `price`, `status`) values ('e3716a2d-f6e4-4e29-9d3d-a2fc820059b6', 'product-1', 10.00, 'VALID');
+insert into product(`id`, `name`, `price`, `status`) values ('cd963607-6e3f-4cac-ad5e-67ab4c77a1cb', 'product-2', 10.00, 'VALID');
+insert into product(`id`, `name`, `price`, `status`) values ('fb4a76e8-b241-4ef2-a427-6b7a394f2450', 'product-3', 10.00, 'VALID');
+insert into product(`id`, `name`, `price`, `status`) values ('5e8e98df-642f-458f-aca5-d22019a8e304', 'product-4', 10.00, 'VALID');
+insert into product(`id`, `name`, `price`, `status`) values ('990c3b2d-627c-4d25-b921-27480d61eafb', 'product-5', 10.00, 'INVALID');
+insert into product(`id`, `name`, `price`, `status`) values ('63c519a4-604e-4ae5-b770-44232a4ada7c', 'product-6', 10.00, 'INVALID');
